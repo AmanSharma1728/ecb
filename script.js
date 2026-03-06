b1926e805f0028b354ecc5749a1788b7562e069b
Aman Sharma <aman.sharma.cse26@gmail.com>
2026-03-06 11:33:28 +0530

    hamburger-menu test-1

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const hamburger = document.querySelector("hamburger");
+const hamburger = document.querySelector(".hamburger");
+const mobile_sidebar = document.querySelector(".mobile-sidebar");
+
 hamburger.addEventListener("click", () => {
-  hamburger.classList.add("active");
+  // hamburger.classList.toggle("active");
+  // mobile_sidebar.classList.toggle("active");
+  console.log("success");
+});
+
+const login_btn = document.querySelector(".login-btn");
+login_btn.addEventListener("click", () => {
+  console.log("logged in");
 });
